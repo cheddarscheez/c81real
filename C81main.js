421ded0e6d2d1f757475b8fd073ef1c2159caeae
cheddarscheez <154029261+cheddarscheez@users.noreply.github.com>
2024-03-24 18:56:34 -0400

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,36 @@
+var canvas = document.getElementById("myCanvas");
+var color="red";
+
+canvas.getContext("2d").beginPath();
+canvas.getContext("2d").strokeStyle = color;
+canvas.getContext("2d").lineWidth = 2;
+canvas.getContext("2d").arc(200, 200, 40, 0, 2*Math.PI);
+canvas.getContext("2d").stroke();
+
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e) {
+    color = document.getElementById("color").value;
+    console.log(color);
+
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+
+    console.log("X = " + mouse_x + " ,Y = " + mouse_y);
+    circle(mouse_x, mouse_y);
+}
+
+function circle(mouse_x, mouse_y) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 2;
+    ctx.arc(mouse_x, mouse_y, 40, 0, 2*Math.PI);
+    ctx.stroke();
+}
